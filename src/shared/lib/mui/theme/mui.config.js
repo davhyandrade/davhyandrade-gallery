29364b9fca361edbfdc8c30605c53b791047e959
davhyandrade <davhyhello@gmail.com>
2025-10-07 15:24:02 -0300

    feat(mui): adds initial Material UI setup

---
+++ src/shared/lib/mui/theme/mui.config.js
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material/styles";
+
+import typography from "./typography";
+
+const theme = createTheme({
+  typography,
+  spacing: 4,
+});
+
+export default theme;
