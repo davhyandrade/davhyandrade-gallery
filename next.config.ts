e08c4656282ad438fdd32f25261bbd38294702fb
davhyandrade <davhyhello@gmail.com>
2025-10-07 14:57:26 -0300

    chore(next.config): adds external URL image permission

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,15 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
