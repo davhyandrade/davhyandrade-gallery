bcb65985470fea658bbdb0c7dd4f4901739e6f97
davhyandrade <davhyhello@gmail.com>
2025-10-10 10:23:35 -0300

    test(Menu): adds unit tests

---
+++ src/app/galeria/components/menu/__tests__/Menu.component.test.tsx
@@ -0,0 +1,54 @@
+import { render, screen } from "@testing-library/react";
+import Menu from "../Menu.component";
+
+let from: string | null = null;
+
+jest.mock("next/navigation", () => ({
+  useRouter: () => ({
+    push: jest.fn(),
+    back: jest.fn(),
+  }),
+  useSearchParams: () => ({
+    get: jest.fn().mockReturnValue(from),
+  }),
+}));
+
+describe("without 'from' parameter", () => {
+  beforeEach(() => {
+    from = null;
+  });
+
+  it("renders button text correclty", () => {
+    render(<Menu />);
+
+    expect(
+      screen.getByRole("button", { name: "Ir para a página principal" })
+    ).toBeInTheDocument();
+  });
+
+  it("renders button icon correclty", () => {
+    render(<Menu />);
+
+    expect(screen.getByTestId("east-icon")).toBeInTheDocument();
+  });
+});
+
+describe("with 'from' parameter", () => {
+  beforeEach(() => {
+    from = "de currículo";
+  });
+
+  it("renders button text correclty", () => {
+    render(<Menu />);
+
+    expect(
+      screen.getByRole("button", { name: "Voltar para a página de currículo" })
+    ).toBeInTheDocument();
+  });
+
+  it("renders button icon correclty", () => {
+    render(<Menu />);
+
+    expect(screen.getByTestId("west-icon")).toBeInTheDocument();
+  });
+});
