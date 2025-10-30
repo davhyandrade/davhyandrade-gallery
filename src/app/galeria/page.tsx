ae5c897f7635bdce11727cfea046ae8dc7e41f48
davhyandrade <davhyhello@gmail.com>
2025-10-29 21:01:54 -0300

    feat(GalleryPage): adds getImages function

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -1,14 +1,18 @@
 import { Box, Stack } from '@mui/material';
+import { getImages } from '@/shared/services/getImages';
 
 import ImageGallery from './components/image-gallery/ImageGallery.component';
-import { initialImages } from './gallery.config';
 
-export default function GalleryPage() {
+async function GalleryPage() {
+  const images = await getImages();
+
   return (
     <Stack width="100%" direction="row" justifyContent="center">
-      <Box maxWidth={1600}>
-        <ImageGallery images={initialImages} />
+      <Box maxWidth={1600} width="100%" >
+        <ImageGallery images={images} />
       </Box>
     </Stack>
   );
 }
+
+export default GalleryPage;
---
+++ src/shared/services/getImages.ts
@@ -0,0 +1,9 @@
+import type { Image } from '@/app/galeria/components/image-gallery/ImageGallery.types';
+import { initialImages } from '@/shared/constants';
+
+export const getImages = (): Promise<Image[]> =>
+  new Promise(resolve => {
+    setTimeout(() => {
+      resolve(initialImages);
+    }, 3000);
+  });
