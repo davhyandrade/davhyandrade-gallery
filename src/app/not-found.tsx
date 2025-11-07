8d12f1539dae006b7c324565d3e372d18abe79a9
davhyandrade <davhyhello@gmail.com>
2025-11-06 22:03:17 -0300

    feat: adds not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,46 @@
+import { Button, Stack, Typography } from '@mui/material';
+
+function NotFoundPage() {
+  return (
+    <Stack
+      width="100%"
+      height="100dvh"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Stack alignItems="center" color="info.main" padding={2}>
+        <Typography variant="h1">404</Typography>
+
+        <Typography
+          variant="body1"
+          color="info.main"
+          textAlign="center"
+          fontWeight={600}
+        >
+          Página não encontrada
+        </Typography>
+
+        <Typography
+          variant="body2"
+          color="info.main"
+          maxWidth={300}
+          textAlign="center"
+          mt={2}
+        >
+          Ela pode ter sido removida ou o endereço está incorreto.
+        </Typography>
+
+        <Button
+          variant="text"
+          color="primary"
+          href="/"
+          sx={{ mt: 8, fontWeight: 600 }}
+        >
+          Voltar para a home
+        </Button>
+      </Stack>
+    </Stack>
+  );
+}
+
+export default NotFoundPage;
