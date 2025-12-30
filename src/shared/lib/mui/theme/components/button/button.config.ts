73ee2e465a067b2a20d81e41cde40da312aa85a9
davhyandrade <davhyhello@gmail.com>
2025-12-30 20:49:46 -0300

    refact(button): changes custom styles

--- src/shared/lib/mui/theme/components/button/button.config.ts
+++ src/shared/lib/mui/theme/components/button/button.config.ts
@@ -1,18 +1,30 @@
 import type { Components, Theme } from '@mui/material/styles';
+import type { ButtonPaletteColor } from './button.types';
+
+const getColorFromOwnerState = (
+  ownerColor: string | undefined,
+): ButtonPaletteColor => {
+  if (ownerColor && ownerColor !== 'inherit') {
+    return ownerColor as ButtonPaletteColor;
+  }
+
+  return 'primary';
+};
 
 export const MuiButton: Components<Theme>['MuiButton'] = {
   styleOverrides: {
     root: {
       textTransform: 'none',
-      fontSize: 14,
+      fontSize: 16,
       fontWeight: 600,
-      borderRadius: 12,
+      borderRadius: 28,
       letterSpacing: 0.3,
-      paddingTop: 12,
-      paddingBottom: 12,
+      paddingTop: 14,
+      paddingBottom: 14,
       paddingLeft: 32,
       paddingRight: 32,
       boxShadow: 'none',
+      transition: 'none',
 
       '&:hover': {
         boxShadow: 'none',
@@ -21,25 +33,37 @@ export const MuiButton: Components<Theme>['MuiButton'] = {
       '& .MuiButton-icon svg': {
         fontSize: 18,
       },
+    },
 
-      variants: [
-        {
-          props: { variant: 'text' },
-          style: {
-            fontSize: 16,
-            fontWeight: 400,
-            padding: 0,
-            paddingLeft: 8,
-            paddingRight: 8,
-            borderRadius: 4,
-            verticalAlign: 'baseline',
+    text: ({ ownerState, theme }) => {
+      const color = getColorFromOwnerState(ownerState.color);
 
-            '&:hover': {
-              textDecoration: 'underline',
-            },
-          },
+      return {
+        fontWeight: 400,
+        padding: 0,
+        paddingLeft: 8,
+        paddingRight: 8,
+        borderRadius: 4,
+
+        '&:hover': {
+          color: theme.palette[color].dark,
+          backgroundColor: 'transparent',
         },
-      ],
+      };
+    },
+
+    outlined: ({ ownerState, theme }) => {
+      const color = getColorFromOwnerState(ownerState.color);
+
+      return {
+        fontWeight: 700,
+
+        '&:hover': {
+          color: theme.palette[color].dark,
+          borderColor: theme.palette[color].dark,
+          backgroundColor: 'transparent',
+        },
+      };
     },
   },
 };
