a437fa11f33c230fb1ee455c65040a00fc4d1887
davhyandrade <davhyhello@gmail.com>
2026-02-01 20:30:47 -0300

    feat: adds react toastify

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,12 +4,14 @@ import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from '@lib/mui/theme/theme.config';
+import ThemedToastContainer from './_providers/themed-toast-container/ThemedToastContainer.component';
 
 function Providers({ children }: { children: React.ReactNode }) {
   return (
     <AppRouterCacheProvider>
       <ThemeProvider theme={theme}>
         <CssBaseline />
+        <ThemedToastContainer />
         {children}
       </ThemeProvider>
     </AppRouterCacheProvider>
